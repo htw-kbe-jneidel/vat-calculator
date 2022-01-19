2614e9bb733e91b6d3910f1f5f5f191736f40439
Jonathan Neidel <jneidel@jneidel.com>
2022-01-19 16:58:13 +0100

    Add utility for manual testing of the queue

---
+++ src/send-test.ts
@@ -0,0 +1,12 @@
+import * as rabbitMq from "./rabbit-mq";
+
+( async () => {
+  const rabbitMqConnection = new rabbitMq.Connection( "127.0.0.1" );
+  await rabbitMqConnection.open();
+  const queue = new rabbitMq.Queue( rabbitMqConnection );
+  await queue.create();
+  const o = { price: 187, category: "küchenzubehör" };
+  const r = await queue.send( "vat-calculator", o );
+  console.log( r );
+  process.exit(0)
+} )();
